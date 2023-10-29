38fe45598fa82de3585f21bf21316115fcbe9d61
Akash <akashsingh2210670@gmail.com>
2023-10-29 15:09:54 +0530

    updated resume

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -4,9 +4,9 @@ const Resume = () => {
   return (
     <div className="mockup-window border bg-base-300 w-full md:w-[450px] h-auto py-5 my-5 md:mx-auto">
       <div className="flex justify-center px-4 py-4 bg-base-200">
-        <a href="https://olive-sarah-98.tiiny.site/">
+        <a href="https://copper-cindie-43.tiiny.site">
           <img
-            src="https://i.ibb.co/TcSf31C/AKASH-SINGH-RESUME-8-10-23.jpg"
+            src="https://i.ibb.co/dc6f52b/AKASH-SINGH-RESUME-29-10-23.jpg"
             alt="AKASH-SINGH-RESUME"
             className=""
           />
