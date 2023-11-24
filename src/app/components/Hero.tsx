c7066f75e40e4587425ab5a2cea632b1056c9123
Akash <akashsingh2210670@gmail.com>
2023-11-24 23:50:30 +0530

    Updated Background image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,8 +8,8 @@ const prata =  Prata({
   })
 const Hero = () => {
   return (
-    <div className={prata.className}>
-    <div className="h-screen bg-hero-bg pt-40  bg-opacity-100 flex flex-col justify-center items-center text-white" data-aos="fade-up">
+    <div className={`${prata.className} backdrop-brightness-50`} >
+    <div className="h-screen bg-hero-bg  bg-cover	 pt-40   bg-opacity-100 flex flex-col justify-center items-center text-white" data-aos="fade-up">
         <p className="text-5xl font-bold py-4 inline">Hi, I'm Akash!</p>
         <p className="text-5xl font-bold py-4 inline">I'm a</p>
         <Typewriter  />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-          'hero-bg':"url('/hero-1.png')",
+          'hero-bg':"url('/hero-2.png')",
       },
     },
   },
