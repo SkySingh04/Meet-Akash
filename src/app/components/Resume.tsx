24ad770fc72c14f7fb8616d8b6b8f177da481f04
Akash <akashsingh2210670@gmail.com>
2023-12-25 00:59:43 +0530

    Updated Work Experience And Resume

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -4,12 +4,8 @@ const Resume = () => {
   return (
     <div className="mockup-window border bg-base-300 w-full md:w-[450px] h-auto py-5 my-5 md:mx-auto">
       <div className="flex justify-center px-4 py-4 bg-base-200">
-        <a href="https://drive.google.com/file/d/1Euje39Xy5Z6fdgiggM3OpIWtRXWMIG3q/view?usp=sharing">
-          <img
-            src="https://i.ibb.co/xJFYnX9/AKASH-SINGH-RESUME-1-12-23.jpg"
-            alt="AKASH-SINGH-RESUME"
-            className=""
-          />
+        <a href="https://drive.google.com/file/d/1evQ0RXs7lrB4Xr1S3ZIUPiapJcYKVE0U/view">
+        <img src="https://i.ibb.co/hV8nnCK/AKASH-SINGH-RESUME-24-12-23.jpg" alt="AKASH-SINGH-RESUME-24-12-23" />
         </a>
       </div>
     </div>
--- src/app/components/WorkExperience.tsx
+++ src/app/components/WorkExperience.tsx
@@ -5,7 +5,7 @@ const WorkExperience = () => {
     <div className="mockup-window border bg-base-300 w-full md:w-[550px] h-auto py-5 md:mx-auto md:mt-[120px]">
       <div className="flex justify-center px-4 py-4 bg-base-200">
         <img
-          src="WorkExperience-1.png"
+          src="WorkExperience-2.png"
           alt="AKASH-SINGH-RESUME"
           className=""
         />
