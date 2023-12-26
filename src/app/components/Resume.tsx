ad64ed86133e1f87db77fb35561b62baa18af1d0
Akash <akashsingh2210670@gmail.com>
2023-12-26 20:05:50 +0530

    Updated Resume

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -4,8 +4,8 @@ const Resume = () => {
   return (
     <div className="mockup-window border bg-base-300 w-full md:w-[450px] h-auto py-5 my-5 md:mx-auto">
       <div className="flex justify-center px-4 py-4 bg-base-200">
-        <a href="https://drive.google.com/file/d/1evQ0RXs7lrB4Xr1S3ZIUPiapJcYKVE0U/view">
-        <img src="https://i.ibb.co/hV8nnCK/AKASH-SINGH-RESUME-24-12-23.jpg" alt="AKASH-SINGH-RESUME-24-12-23" />
+        <a href="https://drive.google.com/file/d/1PLoiMN5M_ycxSh8hfGV0Gtcxg7sXodRq/view?usp=sharing">
+        <img src="https://i.ibb.co/Gxz3rXc/AKASH-SINGH-RESUME-24-12-23.jpg" alt="AKASH-SINGH-RESUME-24-12-23" />
         </a>
       </div>
     </div>
