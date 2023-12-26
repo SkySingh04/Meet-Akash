27a2ce2c53b2016a042c950aaf1d5035aaf1902c
Akash Singh <114267538+Akash-Singh04@users.noreply.github.com>
2023-12-26 10:30:11 +0530

    Update Resume.tsx

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div className="mockup-window border bg-base-300 w-full md:w-[450px] h-auto py-5 my-5 md:mx-auto">
       <div className="flex justify-center px-4 py-4 bg-base-200">
-        <a href="https://drive.google.com/file/d/1evQ0RXs7lrB4Xr1S3ZIUPiapJcYKVE0U/view">
+        <a href="https://drive.google.com/file/d/1SYlLYlw-xv_dUyG-zpNAxbmcyEo0gTxv/view?usp=sharing">
         <img src="https://i.ibb.co/hV8nnCK/AKASH-SINGH-RESUME-24-12-23.jpg" alt="AKASH-SINGH-RESUME-24-12-23" />
         </a>
       </div>
