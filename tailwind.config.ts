964489cd2351224555d2e26529358291e30a7843
Akash <akashsingh2210670@gmail.com>
2023-12-31 18:41:25 +0530

    Updated Backrgound IMG

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-          'hero-bg':"url('/hero-3 compressed.png')",
+          'hero-bg':"url('/hero-3 compressed.webp')",
       },
     },
   },
