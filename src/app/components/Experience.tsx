fb365fd8bfef83e6b8f8d39e4fbbfe735417edff
Akash <akashsingh2210670@gmail.com>
2024-12-17 21:48:56 +0530

    closes #25 updated resume , experience, about me
    Signed-off-by: Akash <akashsingh2210670@gmail.com>

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -3,27 +3,42 @@ import Resume from './Resume';
 import WorkExperience from './WorkExperience';
 
 const Page = () => {
+  const workExperienceImages1 = ['/Vance.png', '/Cloudsek.png' , '/Angelhack.png' , '/Gsoc.png'];
+  const workExperienceImages2 = ['/Swipegen.png', '/Bolesale.png' , '/Codingzen.png'];
+
   return (
-    <div className='page-section text-base px-5 sm:justify-center section-bg' id="Experience" data-aos="fade-up">
-      
+    <div
+      className="page-section text-base px-5 sm:justify-center section-bg"
+      id="Experience"
+      data-aos="fade-up"
+    >
       <div className="md:flex">
         {/* Left Column (Resume) */}
-        <div className="md:w-1/2 p-4 my-4">
-        <div className="section-title ">
-        <h2 className="other-header ">Resume</h2>
-      </div>
+        <div className="md:w-1/3 p-4 my-4">
+          <div className="section-title">
+            <h2 className="other-header">Resume</h2>
+          </div>
           <Resume />
         </div>
 
-        {/* Divider (Visible on Large Screens) */}
+        {/* Middle Column (Work Experience 1) */}
         <div className="hidden md:block border-l border-blue-400 mx-4 my-4"></div>
-
-        {/* Right Column (WorkExperience) */}
-        <div className="md:w-1/2 p-4 my-4 ">
+        <div className="md:w-1/3 p-4 my-4">
           <div className="section-title">
             <h2 className="other-header">Work Experience</h2>
           </div>
-          <WorkExperience />
+          <WorkExperience images={workExperienceImages1} />
+        </div>
+
+        {/* Divider */}
+        <div className="hidden md:block border-l border-blue-400 mx-4 my-4"></div>
+
+        {/* Right Column (Work Experience 2) */}
+        <div className="md:w-1/3 p-4 my-4">
+          <div className="section-title">
+            <h2 className="other-header">Work Experience</h2>
+          </div>
+          <WorkExperience images={workExperienceImages2} />
         </div>
       </div>
     </div>
--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -4,8 +4,8 @@ const Resume = () => {
   return (
     <div className="mockup-window border bg-base-300 w-full md:w-[450px] h-auto py-5 my-5 md:mx-auto">
       <div className="flex justify-center px-4 py-4 bg-base-200">
-        <a href="https://drive.google.com/file/d/1E7RVzIMY4UPVAmDccnIJBktz2LLCgbVw/view?usp=sharing">
-        <Image src="https://i.ibb.co/MMYB89v/AKASH-SINGH-RESUME-12-06-24.jpg" alt="AKASH-SINGH-RESUME-03-05-24" width={1000} height={1000} className="w-full max-h-[100%] object-contain" />
+        <a href="https://drive.google.com/file/d/1ZKGTrFViNGCS5zOASQrw03QHk1BJdRJO/view?usp=sharing">
+        <Image src="https://i.ibb.co/xDzjkVj/AKASH-SINGH-RESUME-17-11-24.jpg" alt="AKASH-SINGH-RESUME" width={1000} height={1000} className="w-full max-h-[100%] object-contain" />
         </a>
       </div>
     </div>
